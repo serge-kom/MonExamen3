8fe8f6123da8576df784a58e6243664db7e09e57
serge <fromsergekom@gmail.com>
2023-04-18 23:00:30 -0400

    class Hotel

---
+++ Hotel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MonExamen3
+{
+    internal class Hotel
+    {
+        public ArrayList clients = new ArrayList();
+
+        public Hotel() { }
+
+        public void ajouterClient(Clients client)
+        {
+            clients.Add(client);
+        }
+    }
+}
